6b4afbfda6d543b44e7830dae2b722c050c307fe
gabriellevitoria <gabriellev912@gmail.com>
2022-10-31 21:26:32 +0000

    aplicação web

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('primeira aplicação web')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')  #toda vez que o flask receber a url com / vai executar a rome. criamos uma rota. mesmo que no final não tenha barra,
+#quando não tem nada ele considera#
+
+def home():
+    return'Hello, Web'
+
+@app.route('/bye')
+def bye():
+    return'Bye'
+
+app.run(debug=True) #fica escutando alterações no código, qualquer mudança reinicia o servidor. 
