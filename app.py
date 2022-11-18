a459a2ca0e0fab8534d99a2b0baff115a40692f6
gabriellevitoria <gabriellev912@gmail.com>
2022-11-18 21:16:30 +0000

    formulários, tasks

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+tasks = [
+    {
+        'name': 'estudar',
+        'finished': False
+    },
+    {
+        'name': 'dormir',
+        'finished': True
+    },
+
+    {   'name' : 'comer',
+        'finished': True
+
+    }
+]
+
 
 @app.route('/')  #toda vez que o flask receber a url com / vai executar a rome. criamos uma rota. mesmo que no final não tenha barra,
 #quando não tem nada ele considera#
 
 def home():
     #templates/home.html
-    return render_template('home.html')
+    return render_template('home.html', tasks = tasks)
 
-@app.route('/bye')
-def bye():
-    return'Bye'
+@app.route('/create', methods =['POST'])
+def create():
+    name = request.form['name']#pega cada input do forms e traz como valor
+    task = {'name': name,
+            'finished': False
+    }
+    tasks.append(task)
+    return render_template('home.html', tasks = tasks) 
 
 app.run(debug=True) #fica escutando alterações no código, qualquer mudança reinicia o servidor. 
